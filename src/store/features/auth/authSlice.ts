7bf23f75ff387eeb105132dfd9ac8edadb9a4710
ThyuHtooAungOwO <thyuhtoo7@gmail.com>
2025-10-09 13:18:35 +0630

    fix: missing closing brace in authSlice

--- src/store/features/auth/authSlice.ts
+++ src/store/features/auth/authSlice.ts
@@ -1,41 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface AuthStateType {
-	user: {
-		id: string;
-		name: string;
-		email: string;
-		role: string;
-		tenent_id?: string;
-	};
-	isAuthenticated?: Boolean;
-	accessToken: string;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+    tenent_id?: string;
+  };
+  isAuthenticated?: Boolean;
+  accessToken: string;
 }
 
 const initialState: AuthStateType = {
-	user: {
-		id: "",
-		name: "",
-		email: "",
-		role: "",
-		tenent_id: "",
-	},
-	isAuthenticated: false,
-	accessToken: "",
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    role: "",
+    tenent_id: "",
+  },
+  isAuthenticated: false,
+  accessToken: "",
 };
 
 export const authSlice = createSlice({
-	name: "auth",
-	initialState,
-	reducers: {
-		login: (state, action) => {
-			state.user = action.payload.user;
-			state.accessToken = action.payload.accessToken;
-			state.isAuthenticated = true;
-		},
-		logout: (state) => {
-			state = initialState;
-	},
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload.user;
+      state.accessToken = action.payload.accessToken;
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state = initialState;
+    },
+  },
 });
 
 export const { login, logout } = authSlice.actions;
