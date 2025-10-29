96e18d26cb7286c87eaf2c8e4ed2bae487c5127d
NaingMinThant77 <naingminthant07@gmail.com>
2025-10-29 15:35:41 +0630

    fix: profileImageUpdate

--- src/pages/my-billing/index.tsx
+++ src/pages/my-billing/index.tsx
@@ -12,27 +12,48 @@ import rentIcon from "@/assets/web_tenants_icons/Property.svg";
 import analyticsIcon from "@/assets/web_tenants_icons/analytics.svg";
 import historyIcon from "@/assets/web_tenants_icons/history.svg";
 import { Badge } from "@/components/ui/badge";
+import { Spinner } from "@/components/ui/spinner";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import CardWrapper from "@/pages/my-billing/components/CardWrapper";
 import OverviewCard from "@/pages/my-billing/components/OverviewCard";
 import BillingHistory from "@/pages/my-billing/components/BillingHistory";
 import TabsContentCard from "@/pages/my-billing/components/TabsContentCard";
-import { BillingCard } from "@/pages/my-billing/components/BillingCard";
 import { getUtilityDataFromBills } from "@/pages/my-billing/utils/billingDataUtils";
 import { getInvoiceTableData } from "@/pages/my-billing/utils/tableDataUtils";
 import { useBillingData } from "@/hooks/use-billing";
-import { FourSquare } from "react-loading-indicators";
 
 // Styles
 const tabTriggerStyle =
   "px-2 py-1 transition data-[state=active]:bg-[#3E70FF] data-[state=active]:text-white text-[#4F4F4F] text-base hover:bg-[#3E70FF]/10 cursor-pointer";
+const commonCard = "bg-[#F5F5F5] border border-[#E0E0E0] rounded-[8px]";
 
-export type Status = "Paid" | "Pending" | "Overdue";
+type Status = "Paid" | "Pending" | "Overdue";
 const statusColors: Record<Status, string> = {
   Paid: "bg-[#58DA8F80] text-[#11321F]",
   Pending: "bg-[#F5D47080] text-[#605020]",
   Overdue: "bg-[#F87171]/20 text-[#B91C1C]",
 };
+
+// BillingCard Component
+interface BillingCardProps {
+  icon: string;
+  title: string;
+  amount: string | number | undefined;
+}
+
+const BillingCard = ({ icon, title, amount }: BillingCardProps) => (
+  <div className={`${commonCard} flex justify-between items-center px-3 py-4`}>
+    <div className="flex gap-3 items-center">
+      <div className="rounded-[8px] bg-[#3E70FF] py-2 px-3">
+        <img src={icon} alt={`${title} Icon`} />
+      </div>
+      <p className="text-lg font-medium text-[#4F4F4F]">{title}</p>
+    </div>
+    <p className="text-xl font-medium text-[#333333]">
+      {Number(amount ?? 0).toLocaleString()} MMK
+    </p>
+  </div>
+);
 
 // Main Billing Component
 const Billing = () => {
@@ -71,43 +92,16 @@ const Billing = () => {
     }
   }, [searchParams, totalPages, currentPage, setSearchParams]);
 
-  if (isLoading) {
-    return (
-      <div className="h-full w-full flex items-center justify-center">
-        <FourSquare
-          color="#2563eb"
-          size="medium"
-          text="Loading Billing Data..."
-          textColor=""
-        />
-      </div>
-    );
-  }
-  if (!history) {
-    return (
-      <div className="h-full w-full flex items-center justify-center">
-        <h2 className="text-2xl font-bold text-primary">
-          No Billing Data For this Tenant Yet!
-        </h2>
-      </div>
-    );
-  }
-  if (isError)
-    return (
-      <div className="h-full w-full flex items-center justify-center">
-        <h2 className="text-2xl font-bold text-primary">
-          Error: {error?.message || "Failed to load data"}
-        </h2>
-      </div>
-    );
+  if (isLoading) return <Spinner className="mx-auto size-6"></Spinner>;
+  if (isError) return <p>Error: {error?.message || "Failed to load data"}</p>;
 
   return (
     <section>
       <div className="flex flex-col gap-6 py-5 lg:py-7 lg:px-4">
         <header className="flex gap-24 items-center lg:gap-0 md:justify-between">
           <div className="flex flex-col gap-3">
-            <h1 className="text-2xl font-bold">My Billing</h1>
-            <p className="text-md">
+            <h1 className="text-h2 text-[#333333]">My Billing</h1>
+            <p className="text-h5 text-[#4F4F4F]">
               Manage your utility bills and payment history
             </p>
           </div>
@@ -165,7 +159,9 @@ const Billing = () => {
             {otherFee && (
               <BillingCard icon={infoIcon} title="Other" amount={otherFee} />
             )}
-            <div className="bg-[#F5F5F5] border border-[#E0E0E0] rounded-lg flex justify-between items-center px-3 py-4">
+            <div
+              className={`${commonCard} flex justify-between items-center px-3 py-4`}
+            >
               <p className="text-[#3E70FF] text-xl lg:text-2xl font-medium">
                 Total Amount Due
               </p>
