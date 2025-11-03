e2d2be81d029c2096afc1ec3ebc8b2965f9c2be8
yehtet <yhtet1934@gmail.com>
2025-11-03 23:38:01 +0800

    Refactor: update profile api types with laravel

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -21,20 +21,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ProfileTab from "./profile-tab";
 import SecurityTab from "./security-tab";
 import LogoutAlert from "@/components/navbar/logout-alert";
-
-type ProfileFieldType = {
-  field: string;
-  value: string;
-};
-
-const ProfileField = ({ field, value }: ProfileFieldType) => {
-  return (
-    <div className="mb-6">
-      <p className="text-h6 text-secondary-foreground">{field}:</p>
-      <p className="text-muted-foreground">{value}</p>
-    </div>
-  );
-};
+import { useFetchRoomQuery } from "@/hooks/use-room";
 
 const profile = () => {
   const navigate = useNavigate();
@@ -45,7 +32,11 @@ const profile = () => {
   const tenantId = useSelector((state: RootState) => state.auth.user?.tenantId);
 
   const { mutate: logout } = useLogout();
-  // const { data: profile, isLoading } = useTenantQuery(tenantId as string);
+  const { tenant, isLoading } = useTenantQuery(tenantId!);
+  const { room } = useFetchRoomQuery(tenant?.roomId!);
+
+  console.log("room: ", room);
+
 
   useEffect(() => {
     if (!isAuthenticated) navigate("/login");
@@ -55,20 +46,29 @@ const profile = () => {
     e.preventDefault();
     logout();
   };
- 
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="h-full w-full flex items-center justify-center">
-  //       <FourSquare
-  //         color="#2563eb"
-  //         size="medium"
-  //         text="Loading Profile..."
-  //         textColor=""
-  //       />
-  //     </div>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div className="h-full w-full flex items-center justify-center">
+        <FourSquare
+          color="#2563eb"
+          size="medium"
+          text="Loading Profile..."
+          textColor=""
+        />
+      </div>
+    );
+  }
+
+  if (!tenant) {
+    return (
+      <div className="h-full w-full flex items-center justify-center">
+        <h2 className="text-2xl font-bold text-primary">
+          No Data For this Tenant Found!
+        </h2>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -80,7 +80,7 @@ const profile = () => {
               <AvatarFallback><User /></AvatarFallback>
             </Avatar>
             <CardTitle>
-              <h1 className="text-h3">Jenny Wilson</h1>
+              <h1 className="text-h3">{tenant?.name}</h1>
               <h3 className="text-h4 text-[#4F4F4F] mb-2">T-0001</h3>
               <Badge className="bg-secondary py-2 px-4">Active</Badge>
             </CardTitle>
@@ -100,7 +100,7 @@ const profile = () => {
               <TabsTrigger value="security"><Shield />Security</TabsTrigger>
             </TabsList>
             <TabsContent value="profile">
-              <ProfileTab />
+              <ProfileTab profile={tenant} />
             </TabsContent>
 
             <TabsContent value="security">
